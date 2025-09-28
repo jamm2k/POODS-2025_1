770a1024e5ee33b8582cbea3e0765c16110c2f94
fredsader <fredrangelsader@gmail.com>
2025-09-28 11:40:41 -0300

    Atualizando pastas backend e frontend

---
+++ backend/src/test/java/br/com/restaurante/gestao_restaurante/GestaoRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restaurante.gestao_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
