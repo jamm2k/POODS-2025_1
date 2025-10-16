de6903e7db6424ab207d7e7c0e4a00d398c99e69
José Arthur de Mello Mulatinho <j4mmbr@gmail.com>
2025-10-16 13:14:43 -0300

    feat: adicionando capacidades de PWA ao projeto

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate', // Mantém o PWA sempre atualizado
+      // Inclui o manifest.json e os ícones
+      manifest: {
+        name: 'Sistema de Restaurante',
+        short_name: 'RestauranteApp',
+        description: 'Sistema de gerenciamento de pedidos para o restaurante.',
+        theme_color: '#ffffff', // Cor da barra de ferramentas do app
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      // Configuração do Service Worker com Workbox
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'] // Define quais arquivos serão cacheados
+      }
+    })
+  ],
 })
