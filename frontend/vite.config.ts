db9af0d8e5e8335d5a71db5e92ffae8be56573ce
José Arthur de Mello Mulatinho <j4mmbr@gmail.com>
2025-11-21 15:33:38 -0300

    fix: corrigindo erro de tipos do TS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,42 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
-    VitePWA({ 
-      registerType: 'autoUpdate', // Mantém o PWA sempre atualizado
-      // Inclui o manifest.json e os ícones
+    VitePWA({
+      disable: mode === 'development', 
+      registerType: 'autoUpdate',
       manifest: {
         name: 'Sistema de Restaurante',
         short_name: 'RestauranteApp',
-        description: 'Sistema de gerenciamento de pedidos para o restaurante.',
-        theme_color: '#ffffff', // Cor da barra de ferramentas do app
+        description: 'Sistema de gerenciamento de pedidos.',
+        theme_color: '#ffffff',
         icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
         ]
       },
-      // Configuração do Service Worker com Workbox
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'] // Define quais arquivos serão cacheados
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
       }
     })
-  ],
-})
+  ]
+}))
